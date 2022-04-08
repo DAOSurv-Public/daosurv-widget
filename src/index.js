d7200462983d9cb25a893c7fe714d9485d0b664e
gitihm <32189851+gitihm@users.noreply.github.com>
2022-04-09 05:23:46 +0700

    remove code unused

--- src/index.js
+++ src/index.js
@@ -3,22 +3,16 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import axios from "axios"
-// Find all widget divs
 
 // Setup Axios once here
 axios.defaults.headers = { Accept: "application/json" }
 const axiosInstance = axios.create({
-  // baseURL: process.env.REACT_APP_BASE_URL,
-  baseURL: " https://swapi.dev/api/",
+  baseURL: process.env.REACT_APP_BASE_URL,
 })
 export default axiosInstance
 
-console.log("axios", axios)
-
 setTimeout(() => {
   let widgetDiv = document.querySelectorAll(".daoalert-widget")
-  console.log("widgetDiv", widgetDiv)
-  console.log("React", React)
 
   // Inject our React App into each class
   widgetDiv.forEach((div) => {
